51103ebb73463cb27faf0effe265d74b2b7abccc
Ol-create <oluola96@gmail.com>
2022-05-02 15:39:07 -0600

    feat: add styles to todo components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 //component file
-import TodoContainer from "./components/TodoContainer"
+import TodoContainer from "./classBased/components/TodoContainer"
 ReactDOM.render(  <React.StrictMode>
     <TodoContainer />
   </React.StrictMode>, document.getElementById("root"))
